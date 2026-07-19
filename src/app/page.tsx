ded8da20163e638dc49c7be805464e37b5fed5f5
Sqidgeon <sqidgeon@outlook.com>
2026-07-19 11:58:35 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,7 +60,6 @@ function Navbar() {
   }, []);
   const links = [
     { href: "/", label: "Home", color: "green", icon: "M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" },
-    { href: "#team", label: "Team", color: "pink", icon: "M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197m13.5-9a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0z" },
   ];
   return (
     <>
